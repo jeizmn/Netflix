b2e9b8976f4456e5c88ca205ca587c32a0e1594b
unknown <jeesmonshaju6@gmail.com>
2023-01-03 19:10:31 +0530

    Netflix

---
+++ src/components/Banner.js
@@ -0,0 +1,43 @@
+import React,{useEffect,useState} from "react";
+import instance from "../baseUrl";
+import './Banner.css'
+
+function Banner({fetchUrl}) {
+    //movies state
+const[movie,setMovies]=useState([])
+
+async function fetchData(){
+    const result=await instance.get(fetchUrl)
+    console.log(result.data.results);
+    setMovies(result.data.results[
+        Math.floor(Math.random()*result.data.results.length-1)
+    ])
+}
+useEffect(()=>{
+    fetchData()
+},[])
+console.log(movie);
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function truncate(str,n){
+    //n=100;
+return str?.length>n ? str.substr(0,n-1)+". . .":str;
+}
+
+  return (
+    <div className="banner" style={
+        {
+            backgroundImage:`url(${base_url}${movie.backdrop_path})`,
+            backgroundSize:"cover",
+            backgroundPosition:"center"
+        }
+    }>
+    <div className="banner_content">
+        <h1 className="banner_title">{movie.name}</h1>
+        <h3 className="banner_desc">{truncate(movie.overview,100)}</h3>
+    </div>
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/components/Row.js
@@ -0,0 +1,34 @@
+import React,{useEffect,useState} from "react";
+import instance from "../baseUrl";
+import './Row.css'
+function Row({title,fetchUrl,isLargeRow}) {
+//movies state
+const[movies,setMovies]=useState([])
+
+async function fetchData(){
+    const result=await instance.get(fetchUrl)
+    console.log(result.data.results);
+    setMovies(result.data.results)
+}
+useEffect(()=>{
+    fetchData()
+},[])
+const base_url = "https://image.tmdb.org/t/p/original/";
+    return(
+    <div className="row">
+        <h2>{title}</h2>
+        <div className="movies">
+            {
+                movies.map(movie=>(
+                <img 
+                className={`movie ${isLargeRow && 'largeMovie'}`}
+                src={`${base_url}${isLargeRow? movie.poster_path:movie.backdrop_path}`}
+                alt={movie.name}
+                />
+                ))
+            }
+        </div>
+    </div>
+    )
+}
+export default Row
